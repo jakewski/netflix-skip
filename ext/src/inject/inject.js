0cb9842ffce94f59bbf3aabe9389fb962cc41d17
Jakub <jakewski@gmail.com>
2017-07-08 16:07:58 -0500

    working with options, a few bugs

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,3 +1,4 @@
+
 chrome.extension.sendMessage({}, function (response) {
 	var readyStateCheckInterval = setInterval(function () {
 		if (document.readyState === "complete") {
@@ -11,25 +12,32 @@ chrome.extension.sendMessage({}, function (response) {
 		}
 	}, 10);
 
-	// let netflix_skip_always = setInterval(() => {
-	// 	let a = document.getElementsByClassName('nf-icon-button')[0], b = document.getElementsByClassName('player-postplay-still-hover-container')[0];
-	// 	a ? a.click() : b ? b.click() : null
-	// }, 1000);
-	console.log('start outer interval')
+	getOptionsAndStartInterval(['skipIntro', 'skipCredits', 'keepWatching']);
+})
+
+function getOptionsAndStartInterval(options) {
+	chrome.storage.sync.get(options, (obj) => {
+		console.log('skipIntro', obj[options[0]])
+		console.log('skipCredits', obj[options[1]])
+		console.log('keepWatching', obj[options[2]])
+
+		startInterval(obj[options[0]], obj[options[1]], obj[options[2]]);
+		
+	})
+}
+
+function startInterval(skipIntroOn, skipCreditsOn, keepWatchingOn) {
 	let isWatching = setInterval(() => {
 		if (window.location.href.match('www.netflix.com/watch')) {
 			console.log("start inner interval")
 			clearInterval(isWatching);
 			let netflix_skip_always = setInterval(() => {
-				console.log("running inner")
+				let skipIntro = document.getElementsByClassName('nf-icon-button')[0],
+					skipCredits = document.getElementsByClassName('player-postplay-still-hover-container')[0],
+					keepWatching = document.getElementsByClassName('continue-playing')[0];
 
-				let a = document.getElementsByClassName('nf-icon-button')[0],
-					b = document.getElementsByClassName('player-postplay-still-hover-container')[0],
-					c = document.getElementsByClassName('continue-playing')[0];
-				a ? a.click() : b ? b.click() : c ? c.click() : null
+				(skipIntroOn && skipIntro) ? skipIntro.click() : (skipCreditsOn && skipCredits) ? skipCredits.click() : (keepWatchingOn && keepWatching) ? keepWatching.click() : null
 			}, 1000);
 		}
 	}, 1000);
-
-
-})
+}
--- ext/src/options/options.js
+++ ext/src/options/options.js
@@ -1,4 +1,39 @@
-const skipIntro = document.getElementById('someSwitchOptionDefault');
+const skipIntro = document.getElementById('skipIntro');
 const skipCredits = document.getElementById('skipCredits');
 const keepWatching = document.getElementById('keepWatching');
-console.log('ye')
+
+setSwitchFromChromeStorage('skipIntro', skipIntro);
+setSwitchFromChromeStorage('skipCredits', skipCredits);
+setSwitchFromChromeStorage('keepWatching', keepWatching);
+
+document.addEventListener('DOMContentLoaded', function () {
+      skipIntro.addEventListener('change', toggleIntro);
+      skipCredits.addEventListener('change', toggleCredits);
+      keepWatching.addEventListener('change', toggleContinue);
+});
+
+function setChromeStorage(switchType, value) {
+    let obj = {};
+    obj[switchType] = value;
+    chrome.storage.sync.set(obj, () => {
+        console.log(switchType, (value ? 'on' : 'off'))
+    })
+}
+
+function setSwitchFromChromeStorage (switchType, toggleSwitch) {
+    chrome.storage.sync.get(switchType, (obj) => {
+		toggleSwitch.checked = obj[switchType];
+	})
+}
+
+const toggleIntro = () => {
+    setChromeStorage('skipIntro', skipIntro.checked);
+}
+
+const toggleCredits = () => {
+    setChromeStorage('skipCredits', skipCredits.checked);
+}
+
+const toggleContinue = () => {
+    setChromeStorage('keepWatching', keepWatching.checked);
+}
